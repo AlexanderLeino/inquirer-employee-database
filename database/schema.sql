a42538e62c5521ddc2e0b4d0cdf3f8df6ec8708e
Alexander Leino <Alexander.Leino@yahoo.com>
2021-10-18 02:18:10 -0400

    tables are ready for use

--- database/schema.sql
+++ database/schema.sql
@@ -4,23 +4,27 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30) UNIQUE NOT NULL
+    id INT AUTO_INCREMENT,
+    department_name VARCHAR(30) UNIQUE NOT NULL,
+    PRIMARY KEY(id)
 );
 
-CREATE TABLE position (
-    id INT UNSIGNED AUTO_INCREMENT,
+CREATE TABLE role (
+    id INT AUTO_INCREMENT,
     title VARCHAR(30),
-    salary DECIMAL,
+    salary DECIMAL NOT NULL,
     department_id INT,
-    FOREIGN KEY(department_id) REFERENCES department(id)
+    PRIMARY KEY(id),
+    CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    id INT UNSIGNED AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    FOREIGN KEY(role_id) REFERENCES position(id),
+    PRIMARY KEY(id),
+    CONSTRAINT fk_role FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE CASCADE,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 ); 
