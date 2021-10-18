d9805621f576976bca4a0a8580b3dbae0c58ede6
Alexander Leino <Alexander.Leino@yahoo.com>
2021-10-18 01:47:32 -0400

    fixing tables

--- database/schema.sql
+++ database/schema.sql
@@ -4,24 +4,23 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
-    department_name VARCHAR(30),
-    PRIMARY KEY (id)
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE position (
-    id INT,
+    id INT UNSIGNED AUTO_INCREMENT,
     title VARCHAR(30),
-    salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
+    salary DECIMAL,
+    department_id INT,
     FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+    id INT UNSIGNED AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     role_id INT,
-    manager_id INT NOT NULL,
+    manager_id INT,
     FOREIGN KEY(role_id) REFERENCES position(id),
 ); 
