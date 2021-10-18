83839b5890ab29fe27dd22d7fe2edaedc91ccbb6
Alexander Leino <Alexander.Leino@yahoo.com>
2021-10-18 01:20:11 -0400

    added tables

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,27 @@
-IF DATABASE EXISTS DELETE employee_db;
+DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
-USE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE position (
+    id INT,
+    title VARCHAR(30),
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY(department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT NOT NULL,
+    FOREIGN KEY(role_id) REFERENCES position(id),
+); 
