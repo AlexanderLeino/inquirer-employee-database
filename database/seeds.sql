86e5e362a8a2f59f1c922dd9c978d2cd119982c6
Alexander Leino <Alexander.Leino@yahoo.com>
2021-10-19 11:35:36 -0400

    added test seeds

---
+++ database/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO department (department_name)
+VALUES ('Test Department'),
+       ('Test Department 2');
+
+INSERT INTO role (title, salary)
+VALUES('Manager', 100000)
+
+INSERT INTO employee(first_name, last_name)
+VALUES ('Alexander', 'Leino')
+
